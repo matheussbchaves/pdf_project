895465c809adf75a3c938ac4e1a7d4d883f07b9d
matheussbchaves <matheussbchaves@gmail.com>
2023-01-29 23:08:53 -0300

    Adiciona if name == main e ajustes menores.

--- scripts/texto_pdf.py
+++ scripts/texto_pdf.py
@@ -1,5 +1,6 @@
 import fitz
 import os
+import pandas as pd
 
 def texto_pdf(arquivo):
     '''Retorna o texto do arquivo PDF(espero)'''
@@ -8,8 +9,11 @@ def texto_pdf(arquivo):
     pag = doc[0]
     texto = pag.get_text()
     linhas = texto.split('\n')
-    
-    return linhas[0]
+    doc.close()
+
+    # arrumar para extrair apenas o texto desejado
+
+    return linhas[0] 
 
 def get_arquivo(path, ext = '.pdf'):
     '''Retorna uma lista com o caminho dos arquivos com a extensão especificada.'''
@@ -23,11 +27,18 @@ def get_arquivo(path, ext = '.pdf'):
 
 
 
-
-
 if __name__ == '__main__':
-    arq = "caminho\\para\\arquivo."
 
+    arq = "caminho\\para\\arquivo."
     path = "caminho\\para\\pasta"
+    xl = "caminho\\para\\planilha.xlsm"
     
-    print(get_arquivo(path))
+    if os.path.exists(xl):
+        arquivos = get_arquivo(path)
+        for arquivo in arquivos:
+            # utilizar função texto_pdf(arquivo) para obter o texto do arquivo;
+            # utilizar função para escrever no excel
+            pass
+
+    else:
+        print('Arquivo não encontrado.') # talvez criar um arquivo novo
